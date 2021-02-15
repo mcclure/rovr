fc6cfa12e2571d53fa09210cceda846a30cdfab7
mcc <andi.m.mcclure@gmail.com>
2021-02-14 19:52:29 -0500

    Switch to mlua, successful lua hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Now I am become Death, the destroyer of worlds");
+use mlua::{Lua};
+use mlua::prelude::LuaError;
+
+fn main() -> Result<(), LuaError> {
+	let lua = Lua::new();
+
+	lua.load(
+        r#"
+            print("Printing from Lua")
+        "#,
+    )
+    .set_name("example code")?
+    .exec()?;
+
+    Ok(())
 }
