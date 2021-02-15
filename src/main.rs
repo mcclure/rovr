0f96c48b3ec3dd4d26d3899b71f280731cbcae36
mcc <andi.m.mcclure@gmail.com>
2021-02-15 01:47:10 -0500

    Version

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
+#![allow(unused_parens)]
+
+use std::env;
 use mlua::{Lua};
 use mlua::prelude::LuaError;
 
 fn main() -> Result<(), LuaError> {
-	let lua = Lua::new();
+	let mut args = env::args().fuse();
+	let _ = args.next();
+	let arg1 = args.next();
 
-	lua.load(
-        r#"
-            print("Printing from Lua")
-        "#,
-    )
-    .set_name("example code")?
-    .exec()?;
+	if (match arg1 {
+		None => false, Some(x) => x == "-v" || x == "--version"
+	}) {
+		println!("ROVR 0.0.1");
+		return Ok(()); // Print version and abort
+	}
+
+	loop { // Run actual program
+		let lua = Lua::new();
+
+		lua.load(
+	        r#"
+	            print("Printing from Lua")
+	        "#,
+	    )
+	    .set_name("example code")?
+	    .exec()?;
+
+	    break;
+	}
 
     Ok(())
 }
