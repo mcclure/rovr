959e63cb669a521f76ad542b5ddbef021120c55e
mcc <andi.m.mcclure@gmail.com>
2021-02-15 23:25:54 -0500

    Handle arg, convert tabs to spaces

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,53 @@ use mlua::{Lua};
 use mlua::prelude::LuaError;
 
 fn main() -> Result<(), LuaError> {
-	let mut args = env::args().fuse();
-	let _ = args.next();
-	let arg1 = args.next();
+    let mut args = env::args().fuse();
+    let arg0 = args.next();
+    let arg1 = args.next();
 
-	if (match arg1 {
-		None => false, Some(x) => x == "-v" || x == "--version"
-	}) {
-		println!("ROVR 0.0.1");
-		return Ok(()); // Print version and abort
-	}
+    if (match arg1 {
+        None => false, Some(ref x) => x == "-v" || x == "--version"
+    }) {
+        println!("ROVR 0.0.1");
+        return Ok(()); // Print version and abort
+    }
 
-	loop { // Run actual program
-		let lua = Lua::new();
+    // FIXME handle --console
 
-		lua.load(
-	        r#"
-	            print("Printing from Lua")
-	        "#,
-	    )
-	    .set_name("example code")?
-	    .exec()?;
+    loop { // Run actual program
+        let lua = Lua::new();
 
-	    break;
-	}
+        // TODO: luax_setmainthread(L);
+
+        let arg_table = lua.create_table()?;
+        
+        match arg0 { None => (), Some(x) => arg_table.set("exe", x)? }
+        match arg1 { None => (), Some(x) => arg_table.set(0, x)? }
+
+        // FIXME set "cookie"
+        {
+            let mut idx = 1;
+            for arg in args {
+                arg_table.set(idx, arg)?;
+                idx += 1;
+            }
+        }
+
+        let globals = lua.globals();
+        globals.set("arg", arg_table)?;
+
+        // TODO lovrModules
+
+        lua.load(
+            r#"
+                print("Printing from Lua", arg[1])
+            "#,
+        )
+        .set_name("example code")?
+        .exec()?;
+
+        break;
+    }
 
     Ok(())
 }
